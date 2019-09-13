ef63d1f59749908c5fcef75abc6048ca62fa9be1
Eric Robksy Huntley <ehuntley@mit.edu>
2019-09-13 16:22:54 -0400

    add alt allowed hosts

--- prof_site/prof_site/settings.py
+++ prof_site/prof_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.ericrobskyhuntley.org', 'localhost']
+ALLOWED_HOSTS = ['.ericrobskyhuntley.org', 'ericrobskyhuntley.org', 'www.ericrobskyhuntley.org', 'localhost']
 
 # Application definition
 

e69bfe65c0b4c27e5ad933b8e68c74cf963e0820
Eric Robsky Huntley <ehuntley@mit.edu>
2021-02-15 21:51:31 -0500

    add affiliation inline

--- prof_site/blog/admin.py
+++ prof_site/blog/admin.py
@@ -1,10 +1,18 @@
+from django.contrib import admin
 from django.contrib.gis import admin
 from markdownx.admin import MarkdownxModelAdmin
 
 from .models import Author, Post, Institution, Affiliation, UrbanArea, CitationStyle
 
+class AffiliationInline(admin.TabularInline):
+    model = Affiliation
+    extra = 1
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = (AffiliationInline,)
+
 # Register your models here.
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Affiliation)
 admin.site.register(UrbanArea)
 admin.site.register(Institution, admin.OSMGeoAdmin)

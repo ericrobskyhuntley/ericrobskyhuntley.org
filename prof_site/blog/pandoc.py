be1c43b28b754e65cea7f0fc5ed5356d06050ca4
Eric Robsky Huntley <ehuntley@mit.edu>
2022-05-17 11:21:40 -0400

    fix line break after references

--- prof_site/blog/pandoc.py
+++ prof_site/blog/pandoc.py
@@ -9,20 +9,20 @@ top-level-division: section
 """
 
 def pandocify(content, csl, bib):
-    if bib:
+    if bib and csl:
         filters = []
         extra_args = ['--mathjax',
                 '--citeproc',
                 '--bibliography='+bib,
                 '--csl='+csl]
-        content = content + "<br> <h3>References</h3>"
+        content = content + "\n\n### References"
     else:
         filters = []
         extra_args = ['--mathjax']
     return pypandoc.convert_text(
         yaml + content,
         'html5',
-        format='md',
+        format = 'md',
         extra_args = extra_args,
         filters = filters
     )
